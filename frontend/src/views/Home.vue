c63226c36bdd2ab544675c2cdc6b5a38da62c0b7
Dee-Why <58548803+Dee-Why@users.noreply.github.com>
2021-05-09 16:23:53 +0800

    change name into PkuWenWen

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="PkuWenWen logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to PkuWenWen"/>
   </div>
 </template>
 
