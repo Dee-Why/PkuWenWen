374efaeab353268e1c6be131a574f0701eb9c3ec
Dee-Why <by_liumingchao@pku.edu.cn>
2021-05-11 13:03:06 +0800

    Add element-plus into the node package

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,4 +2,13 @@
   <div class="about">
     <h1>This is the about page of PkuWenWen</h1>
   </div>
+  <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
+
 </template>
