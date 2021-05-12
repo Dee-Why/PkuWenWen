0f192cd648c627fc8930e968d505abaf45ee456f
Dee-Why <by_liumingchao@pku.edu.cn>
2021-05-12 16:19:03 +0800

    implementing element-plus

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/sign_up">Sign Up</router-link>
+  </div>
+  <div id="element-ui-test">
     <p>
       If Element Plus is successfully added to this project, you'll see an
       <code v-text="'<el-button>'"></code>
@@ -8,27 +13,30 @@
     </p>
     <el-button type="primary">el-button</el-button>
   </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
   margin-top: 60px;
 }
+
 </style>
