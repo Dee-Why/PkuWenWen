3ec92546a5b16feb221c5deab4dad9012bf836d4
Dee-Why <by_liumingchao@pku.edu.cn>
2021-05-12 16:06:45 +0800

    install element-plus and try'

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,8 @@ import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
